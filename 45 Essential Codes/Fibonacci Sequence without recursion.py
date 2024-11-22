b92261a460320678f3b31bfd240ee88b6e944f69
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2024-11-22 14:04:44 +0530

    Add files via upload

---
+++ 45 Essential Codes/Fibonacci Sequence without recursion.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    first=0
+    second=1
+    fibb=[first,second]
+    for i in range(2,n):
+        next= first + second
+        fibb.append(next)
+        first = second
+        second = next
+    return fibb
+
+n=9
+res=fibonacci(n)
+print(res)
+
+    
+
+
